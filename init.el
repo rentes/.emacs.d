09b4a723076b8c2272052a9349609876dc3ed611
Miguel Rentes <miguel.rentes@gmail.com>
2012-08-02 16:15:03 +0100

    Separating init.el into 2 different files: custom.el and my-custom.el
    custom.el is where customizations with the 'customize' interface will be
    added, and my-custom.el is where I will keep my local customizations.

--- init.el
+++ init.el
@@ -1,35 +1,17 @@
-; encoding stuff
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-; removing Byte Order Mark (BOM)
-(set-buffer-file-coding-system 'unix)
+; -*- coding: utf-8 -*-
+; ==========================================================================
+; load-path
+; ==========================================================================
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/"))
 
-; load-paths
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-; theme path
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-; other customizations
-(setq auto-save-default nil)
-(delete-selection-mode 1)
-(global-hl-line-mode 1)
-(global-linum-mode 1)
-(column-number-mode 1)
-(setq show-paren-delay 0)
-(show-paren-mode t)
-(setq show-paren-style 'parenthesis)
+; ==========================================================================
+; Variables configured via the interactive 'customize' interface
+; get recorded on custom.el file
+; ==========================================================================
+(setq custom-file "custom.el")
+(load custom-file)
 
-(setq frame-title-format
-  '("emacs%@" (:eval (system-name)) ": " (:eval (if (buffer-file-name)
-                (abbreviate-file-name (buffer-file-name))
-                  "%b")) " [%*]"))
-
-;; turn on font-lock mode
-(when (fboundp 'global-font-lock-mode)
-  (global-font-lock-mode t))
-
-;; marmalade
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")))
+; ==========================================================================
+; my customizations
+; ==========================================================================
+(load "my-custom.el")
---
+++ my-custom.el
@@ -0,0 +1,43 @@
+; ==========================================================================
+; encodings
+; ==========================================================================
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-buffer-file-coding-system 'unix) ; removing Byte Order Mark (BOM)
+
+; ==========================================================================
+; other customizations
+; ==========================================================================
+(setq auto-save-default nil)
+(delete-selection-mode 1)
+(global-hl-line-mode 1)
+(global-linum-mode 1)
+(column-number-mode 1)
+(setq show-paren-delay 0)
+(show-paren-mode t)
+(setq show-paren-style 'parenthesis)
+
+; ==========================================================================
+; change frame title to include complete path on open file
+; ==========================================================================
+(setq frame-title-format
+      '("emacs%@" (:eval (system-name)) ": " 
+	(:eval (if (buffer-file-name)
+		   (abbreviate-file-name (buffer-file-name))
+		 "%b")) " [%*]"))
+
+; ==========================================================================
+; turn on font-lock mode (syntax highlighting)
+; ==========================================================================
+(when (fboundp 'global-font-lock-mode)
+  (global-font-lock-mode t))
+
+; ==========================================================================
+; An alist of archives from which to fetch
+; ==========================================================================
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("elpa" . "http://tromey.com/elpa/")))
