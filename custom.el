6207086fc453d376a4b0804a63344dc3225d40bd
Miguel Rentes <miguel.rentes@gmail.com>
2013-05-16 22:00:12 +0100

    Replacing my-custom.el by custom.el for simplicity

--- custom.el
+++ custom.el
@@ -1,12 +1,47 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Profont")))))
+; ==========================================================================
+; encodings
+; ==========================================================================
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-buffer-file-coding-system 'unix) ; removing Byte Order Mark (BOM)
+; ==========================================================================
+; other customizations
+; ==========================================================================
+(setq auto-save-default nil)
+(delete-selection-mode 1)
+(global-hl-line-mode 1)
+(global-linum-mode 1)
+(column-number-mode 1)
+(setq show-paren-delay 0)
+(show-paren-mode t)
+(setq show-paren-style 'parenthesis)
+; ==========================================================================
+; change frame title to include complete path on open file
+; ==========================================================================
+(setq frame-title-format
+      '("emacs%@" (:eval (system-name)) ": " 
+	(:eval (if (buffer-file-name)
+		   (abbreviate-file-name (buffer-file-name))
+		 "%b")) " [%*]"))
+
+; ==========================================================================
+; turn on font-lock mode (syntax highlighting)
+; ==========================================================================
+(when (fboundp 'global-font-lock-mode)
+  (global-font-lock-mode t))
+; ==========================================================================
+; An alist of archives from which to fetch
+; ==========================================================================
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("elpa" . "http://tromey.com/elpa/")))
+; ==========================================================================
+; bind keys customization
+; ==========================================================================
+; here is a nice cat!
+; see https://github.com/afroisalreadyinu/gimme-cat for more info
+(require 'gimme-cat)
+(global-set-key "\C-c\k" 'gimme-cat)
