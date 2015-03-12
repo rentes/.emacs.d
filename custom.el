de3cb4318560e6aed0c3361e62e7186623e22705
Miguel Rentes <miguel.rentes@gmail.com>
2015-03-12 22:36:01 +0000

    custom.el: remove proxy settings and uninstalled dependencies errors

--- custom.el
+++ custom.el
@@ -53,18 +53,10 @@
 ; tramp settings
 ; ==========================================================================
 (setq tramp-verbose 10)
-;(setq tramp-shell-prompt-pattern "^[^$>\n]*[#$%>] *\\(\[[0-9;]*[a-zA-Z] *\\)*")
-;(set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:scm@%h:"))))
 ; ==========================================================================
 ; load library
 ; ==========================================================================
 (load-library "iso-transl")
-; ==========================================================================
-; proxy settings
-; ==========================================================================
-(setq url-proxy-services '(("no_proxy" . "127.0.0.1, 172.18.200.0/24, 172.18.210.0/24, 172.18.214.0/24")
-			   ("http" . "proxyarrsec.efacec.pt:8080")
-			   ("https" . "proxyarrsec.efacec.pt:8080")))
 ; ==========================================================================
 ; python jedi
 ; ==========================================================================
@@ -73,68 +65,24 @@
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
 
-; ==========================================================================
-; org-mode latex
-; ==========================================================================
-(require 'org-latex)
-(unless (boundp 'org-export-latex-classes)
-  (setq org-export-latex-classes nil))
-(add-to-list 'org-export-latex-classes
-	     '("article"
-	       "\\documentclass{article}"
-	       ("\\section{%s}" . "\\section*{%s}")
-	       ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-
 (setq-default fill-column 80)
-;(whitespace-mode 1)
-
-;; (set-face-attribute 'mode-line nil
-;;     :foreground "black" :background "orange"
-;;     :inverse-video nil :height 100
-;;     :box '(:line-width 1 :color "white" :style nil))
 
 ; ==========================================================================
 ; load theme
 ; ==========================================================================
-;(load-theme 'solarized-light t)
-;(load-theme 'solarized-dark t)
-;(load-theme 'waher t)
-(load-theme 'ample-zen t)
-
-; ==========================================================================
-; w3m settings - see http://www.emacswiki.org/emacs/emacs-w3m for more info
-; ==========================================================================
-;; (require 'w3m-e21)
-;; (provide 'w3m-e23)
-(require 'w3m-load)
-(setq browse-url-browser-function 'w3m-browse-url)
- (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
- ;; optional keyboard short-cut
- (global-set-key "\C-xm" 'browse-url-at-point)
- ;; enabling cookies
-(setq w3m-use-cookies t)
-
+(load-theme 'solarized-dark t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("0f0e3af1ec61d04ff92f238b165dbc6d2a7b4ade7ed9812b4ce6b075e08f49fe" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "7a9f392481b6e2fb027ab9d8053ab36c0f23bf5cc1271206982339370d894c74" "a37600b047da389eccc4a17b5f165d512fb1d32f18d93cffb28154b5f4eb4437" default))))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 123 :width normal :foundry "unknown" :family "Terminus"))))
- '(org-level-1 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.0 :family "Terminus"))))
- '(org-level-2 ((t (:inherit variable-pitch :foreground "#859900" :height 0.9 :family "Terminus"))))
- '(org-level-3 ((t (:inherit variable-pitch :foreground "#268bd2" :height 0.85 :family "Terminus"))))
- '(org-level-4 ((t (:inherit variable-pitch :foreground "#b58900" :height 0.8 :family "Terminus"))))
- '(org-level-5 ((t (:inherit variable-pitch :foreground "#2aa198" :height 0.75 :family "Terminus"))))
- '(org-level-6 ((t (:inherit variable-pitch :foreground "#859900" :height 0.7 :family "Terminus"))))
- '(org-level-7 ((t (:inherit variable-pitch :foreground "#dc322f" :height 0.65 :family "Terminus"))))
- '(org-level-8 ((t (:inherit variable-pitch :foreground "#268bd2" :height 0.6 :family "Terminus")))))
+ )
