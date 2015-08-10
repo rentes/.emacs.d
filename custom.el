76d38463564162b57de2df2597c43a68a43cc1aa
Miguel Rentes <miguel.rentes@gmail.com>
2015-08-10 16:47:25 +0100

    Updates custom.el and melpa archive contents

--- custom.el
+++ custom.el
@@ -29,6 +29,7 @@
 ;; Enable copy and pasting from clipboard
 (setq x-select-enable-clipboard t)
 
+(load-library "iso-transl")
 ; ====================================================================
 ; change frame title to include complete path on open file
 ; ====================================================================
@@ -62,11 +63,8 @@
 ; tramp settings
 ; ====================================================================
 (setq tramp-verbose 10)
+(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
 (setq tramp-default-method "ssh")
-; ====================================================================
-; load library
-; ====================================================================
-(load-library "iso-transl")
 
 ; ====================================================================
 ; org-mode workflow states
@@ -211,7 +209,11 @@ If the file is emacs lisp, run the byte compiled version if exist."
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("ad86686d808c718244a9a43e073eb520f5304bef8d4969269d7efe873b05272b" "40134952d3c31bd2494264f3702fac19f9f26f8f9cb6f4cc641d77a3c93b66f8" default))))
+    ("ad86686d808c718244a9a43e073eb520f5304bef8d4969269d7efe873b05272b" "40134952d3c31bd2494264f3702fac19f9f26f8f9cb6f4cc641d77a3c93b66f8" default)))
+ '(tramp-default-method "ssh")
+ '(tramp-ssh-controlmaster-options
+   "-o ControlMaster=auto -o ControlPath='~/.ssh/controlmasters/%r@%h:%p' -o ControlPersist=10m")
+ '(tramp-verbose 10))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
